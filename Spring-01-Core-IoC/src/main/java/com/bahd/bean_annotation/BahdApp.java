6bc264f776af1df8702d9f446a5c21f84c728177
Bahadir Turgut <bturgutny@gmail.com>
2023-09-01 15:43:09 -0400

    Bean Annotation

---
+++ Spring-01-Core-IoC/src/main/java/com/bahd/bean_annotation/BahdApp.java
@@ -0,0 +1,18 @@
+package com.bahd.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BahdApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class, ConfigAny.class);
+
+        FullTimeMentor ft = container.getBean(FullTimeMentor.class);
+
+        ft.createAccount();
+
+        String st = container.getBean(String.class);
+        System.out.println(st);
+
+    }
+}
