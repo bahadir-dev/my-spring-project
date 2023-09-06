6a7e5adee79e3efacfc16a8469d1994d7b4ef17f
Bahadir Turgut <bturgutny@gmail.com>
2023-09-05 22:19:07 -0400

    Spring MVC Controller-update

--- Spring-07-Spring-MVC-Controller/src/main/java/com/bahd/controller/HomeController.java
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/bahd/controller/HomeController.java
@@ -3,12 +3,13 @@ package com.bahd.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller //Annotate the class with the @Controller stereotype annotation
 public class HomeController {
 
+    //Use @ReqestMapping annotation to associate the action with an HTTP request path
     @RequestMapping("/home")
     public String home(){
-        return "home.html";
+        return "home.html"; //Return the HTML doc name that contains the details we want the browser display
     }
 
     @RequestMapping("/welcome")
