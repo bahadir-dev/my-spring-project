1ecedc29434a29363c2c9b10dcae0d6d533c278b
Bahadir Turgut <bturgutny@gmail.com>
2023-09-06 23:25:54 -0400

    Spring MVC Model-Task

--- Spring-08-Spring-MVC-Model/src/main/java/com/bahd/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/bahd/controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.bahd.controller;
 
+import com.bahd.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class StudentController {
@@ -13,6 +18,23 @@ public class StudentController {
         //model methods
         model.addAttribute("name","Bahadir");
         model.addAttribute("course","MVC");
+
+        String subject = "Collections";
+        model.addAttribute("subject",subject);
+
+        //create some random studentId (0-1000) and show it in your UI
+        int studentId =new Random().nextInt(1000);
+        model.addAttribute("id",studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(7);
+        numbers.add(11);
+
+        model.addAttribute("numbers", numbers);
+
+        Student student = new Student(1,"Mike","Truner");
+        model.addAttribute("student",student);
 
         return "student/welcome";
     }
