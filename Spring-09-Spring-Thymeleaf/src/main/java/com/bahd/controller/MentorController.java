56cbcfc6f2adb67c9fd6414feccd9f96e0ee7ae7
Bahadir Turgut <bturgutny@gmail.com>
2023-10-20 21:47:26 -0400

    ORM start

--- Spring-09-Spring-Thymeleaf/src/main/java/com/bahd/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/bahd/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.bahd.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -23,5 +25,15 @@ public class MentorController {
         model.addAttribute("batchList",batchList);
 
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("confirm")
+    public String showForm2(@ModelAttribute("mentor") Mentor mentor){
+
+        System.out.println(mentor.toString());
+
+
+       // return "mentor/mentor-confirmation";
+        return "redirect:/mentor/register";
     }
 }
