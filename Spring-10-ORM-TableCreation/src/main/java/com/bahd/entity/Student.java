b0905af1fb7d1a06656c61b4b77aed8a38fba021
Bahadir Turgut <bturgutny@gmail.com>
2023-10-21 09:55:50 -0400

    ORM entity anotations

--- Spring-10-ORM-TableCreation/src/main/java/com/bahd/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/bahd/entity/Student.java
@@ -1,15 +1,37 @@
 package com.bahd.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.bahd.enums.Gender;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
+@Table(name="students")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="dtudentFirstName")
     private String firstName;
+    @Column(name="studentLastName")
     private String lastName;
     private String email;
+
+    @Transient
+    private String city;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthDate;
+    @Column(columnDefinition = "TIME")
+    private LocalTime birthTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDate birthDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+
 }
---
+++ Spring-10-ORM-TableCreation/src/main/java/com/bahd/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.bahd.enums;
+
+public enum Gender {
+    MALE, FEMALE;
+}
