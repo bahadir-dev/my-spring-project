90c108105bdff9e4c638c0a94093a84f8d78a575
Bahadir Turgut <bturgutny@gmail.com>
2023-11-07 18:30:35 -0500

    course entity add

--- Spring-14-ORM-Queries/src/main/java/com/bahd/bootstrap/DataGenerator.java
+++ Spring-14-ORM-Queries/src/main/java/com/bahd/bootstrap/DataGenerator.java
@@ -1,5 +1,6 @@
 package com.bahd.bootstrap;
 
+import com.bahd.repository.CourseRepository;
 import com.bahd.repository.DepartmentRepository;
 import com.bahd.repository.EmployeeRepository;
 import com.bahd.repository.RegionRepository;
@@ -12,12 +13,15 @@ public class DataGenerator implements CommandLineRunner {
     private final RegionRepository regionRepository;
     private final DepartmentRepository departmentRepository;
     private final EmployeeRepository employeeRepository;
+    private final CourseRepository courseRepository;
 
-    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
+    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository, CourseRepository courseRepository) {
         this.regionRepository = regionRepository;
         this.departmentRepository = departmentRepository;
         this.employeeRepository = employeeRepository;
+        this.courseRepository = courseRepository;
     }
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -46,5 +50,12 @@ public class DataGenerator implements CommandLineRunner {
 
 
         System.out.println("--------EMPLOYEE END--------");
+
+        System.out.println("--------COURSE START--------");
+        courseRepository.findByCategory("Spring").forEach(System.out::println);
+        courseRepository.findByCategoryOrderByName("Spring").forEach(System.out::println);
+        System.out.println(courseRepository.existsByName("JavaScript"));
+        System.out.println(courseRepository.countByCategory("Spring"));
+        System.out.println("--------COURSE END--------");
     }
 }
---
+++ Spring-14-ORM-Queries/src/main/java/com/bahd/repository/CourseRepository.java
@@ -0,0 +1,28 @@
+package com.bahd.repository;
+
+import com.bahd.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public interface CourseRepository extends JpaRepository<Course,Long> {
+
+    //find all courses by catgory
+    List<Course> findByCategory(String category);
+
+    //FIND all courses by catggory and order the entities by name
+    List<Course> findByCategoryOrderByName(String category);
+
+    //check if a course with provided name exists, return true if exists
+    boolean existsByName(String name);
+
+    //returns the count of courses fro the provided category
+    int countByCategory(String category);
+
+    //find all courses that start with the provided course name
+    List<Course> findByNameStartingWith(String pattern);
+
+    //find all courses by category and returns a stream
+    Stream<Course> streamByCategory(String category);
+}
