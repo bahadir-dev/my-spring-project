b138c20f1a4737bdde5b3610b4f18f518dcc0efd
Bahadir Turgut <bturgutny@gmail.com>
2023-11-07 22:43:52 -0500

    JPQL queries with name params

--- Spring-14-ORM-Queries/src/main/java/com/bahd/repository/DepartmentRepository.java
+++ Spring-14-ORM-Queries/src/main/java/com/bahd/repository/DepartmentRepository.java
@@ -2,6 +2,7 @@ package com.bahd.repository;
 
 import com.bahd.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -16,5 +17,11 @@ public interface DepartmentRepository extends JpaRepository<Department,String> {
 
     //display top 3 depts with division name includes "Hea" without duplicates
     List<Department> findDistinctTop3ByDivisionContains(String pattern);
+
+    // get departments in divisions
+    @Query("SELECT d FROM Department d WHERE d.division IN ?1")
+    List<Department> getDepartmentDivision(List<String> division);
+
+
 
 }
