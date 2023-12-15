21eeb07b44f915a3cae7fecef1e8438f1f2e8d27
Bahadir Turgut <bturgutny@gmail.com>
2023-12-14 22:36:01 -0500

    new module & pojo classes

---
+++ Spring-17-REST-ConsumingAPIs/src/test/java/com/bahd/Spring17RestConsumingApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring17RestConsumingApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
