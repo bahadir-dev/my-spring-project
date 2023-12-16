2928430f9d1ce812c9e5d612d008ed4a72a328ef
Bahadir Turgut <bturgutny@gmail.com>
2023-12-16 09:05:08 -0500

    Feign client

--- Spring-17-REST-ConsumingAPIs/src/main/java/com/bahd/Spring17RestConsumingApIsApplication.java
+++ Spring-17-REST-ConsumingAPIs/src/main/java/com/bahd/Spring17RestConsumingApIsApplication.java
@@ -1,11 +1,18 @@
 package com.bahd;
 
+import feign.Client;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.cloud.openfeign.FeignContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class Spring17RestConsumingApIsApplication {
 
 	public static void main(String[] args) {
@@ -16,4 +23,5 @@ public class Spring17RestConsumingApIsApplication {
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
+
 }
