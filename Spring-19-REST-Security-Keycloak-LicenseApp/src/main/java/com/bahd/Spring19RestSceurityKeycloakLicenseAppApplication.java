f02b2e9e146ce00e8c800d22844aaca6f6082299
Bahadir Turgut <bturgutny@gmail.com>
2023-12-30 00:44:18 -0500

    Keycloak module start

---
+++ Spring-19-REST-Security-Keycloak-LicenseApp/src/main/java/com/bahd/Spring19RestSceurityKeycloakLicenseAppApplication.java
@@ -0,0 +1,13 @@
+package com.bahd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring19RestSceurityKeycloakLicenseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring19RestSceurityKeycloakLicenseAppApplication.class, args);
+    }
+
+}
