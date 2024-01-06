65da617c6882993589d3d82fb240183f4b3d4bea
Bahadir Turgut <bturgutny@gmail.com>
2024-01-05 21:19:38 -0500

    aspect class

--- Spring-20-AOP/src/main/java/com/bahd/aspect/LoggingAspect.java
+++ Spring-20-AOP/src/main/java/com/bahd/aspect/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.bahd.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -17,9 +18,23 @@ public class LoggingAspect {
     @Pointcut("execution(* com.bahd.controller.CourseController.*(..))")
     private void pointcut(){}
 
-    @Before("pointcut()")
-    public void log(){
+//    @Before("pointcut()")
+//    public void log(){
+//        logger.info("Logger info --------");
+//    }
+
+    //both of above can be combined in this way
+    @Before("execution(* com.bahd.controller.CourseController.*(..))")
+    public void beforeAdvice(){
         logger.info("Logger info --------");
+    }
+
+    @Pointcut("execution(* com.bahd.repository.CourseRepository.findById(*))")
+    private void anyProductRepositoryFindById(){}
+
+    @Before("anyProductRepositoryFindById()")
+    public void beforeCourseRepoOperation(JoinPoint joinPoint){
+        logger.info("Before (findById): -> Method: {} - Arguments: {} - Target: {}", joinPoint, joinPoint.getArgs(), joinPoint.getTarget());
     }
 
 }
