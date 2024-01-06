814861f572fcf0c282266960ee5438758ce1ddee
Bahadir Turgut <bturgutny@gmail.com>
2024-01-06 08:54:28 -0500

    Annotation, afterReturning, afterThrowing

--- Spring-20-AOP/src/main/java/com/bahd/aspect/LoggingAspect.java
+++ Spring-20-AOP/src/main/java/com/bahd/aspect/LoggingAspect.java
@@ -1,12 +1,12 @@
 package com.bahd.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 
 @Aspect
@@ -48,5 +48,35 @@ public class LoggingAspect {
     public void beforeControllerAdvice(JoinPoint joinPoint){
         logger.info("Before (findById): -> Method: {} - Arguments: {} - Target: {}", joinPoint, joinPoint.getArgs(), joinPoint.getTarget());
     }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+    private void anyDeleteCourseOperation(){}
+
+    @Before("anyDeleteCourseOperation()")
+    public void beforeControllerAdvice2(JoinPoint joinPoint){
+        logger.info("Before (findById): -> Method: {} - Arguments: {} - Target: {}", joinPoint, joinPoint.getArgs(), joinPoint.getTarget());
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    private void anyGetCourseOperation(){}
+
+//    @AfterReturning(pointcut = "anyGetCourseOperation()", returning = "result")
+//    public void afterReturningContollerAdvice(JoinPoint joinPoint, Object result){
+//        logger.info("After returning -> Method: {} - result:{]", joinPoint.getSignature().toShortString(), result.toString());
+//    }
+
+    //return list
+    @AfterReturning(pointcut = "anyGetCourseOperation()", returning = "result")
+    public void afterReturningContollerAdvice(JoinPoint joinPoint, List<Object> result){
+        logger.info("After returning(List) -> Method: {} - result:{]", joinPoint.getSignature().toShortString(), result.toString());
+    }
+
+    @AfterThrowing(pointcut = "anyGetCourseOperation()", throwing = "exception")
+    public void afterThrowingControllerAdvice(JoinPoint joinPoint, RuntimeException exception){
+        logger.info("After returning -> Method: {} - Exception:{]", joinPoint.getSignature().toShortString(), exception.toString());
+    }
+
+
+
 
 }
