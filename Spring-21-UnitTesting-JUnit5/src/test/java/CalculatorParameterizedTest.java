7ffcb37ac89c7dff52b72a620897f98cf1b7ce4d
Bahadir Turgut <bturgutny@gmail.com>
2024-01-09 22:25:38 -0500

    Test with value sources

--- Spring-21-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
+++ Spring-21-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 public class CalculatorParameterizedTest {
 
@@ -9,7 +9,6 @@ public class CalculatorParameterizedTest {
     @ValueSource(strings = {"abc1","abc2","ced3"})
     void testCase1(String args){
         Assertions.assertTrue(!args.isEmpty());
-
     }
 
     @ParameterizedTest
@@ -18,4 +17,32 @@ public class CalculatorParameterizedTest {
         Assertions.assertEquals(0 , number % 3);
 
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc1","abc2","ced3"})
+//    @EmptySource
+//    @NullSource
+    @NullAndEmptySource
+    void testCase3(String args){
+        Assertions.assertTrue(!args.isEmpty());
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringProvider")
+    void testCas4(String arg){
+        Assertions.assertNotNull(arg);
+    }
+
+    static String[] stringProvider(){
+        return new String[]{"Java", "JS", "Ruby"};
+    }
+
+    @ParameterizedTest
+    @CsvSource({"10,20,30", "20,20,40", "10,30,50"})
+    void testCase5(int num1, int num2, int result){
+        Assertions.assertEquals(result, Calculator.add(num1, num2));
+    }
+
+
+
 }
